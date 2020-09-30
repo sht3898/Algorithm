ce2900c44ed29cbc39fcf51825369e1325e76bac
sht3898 <sehyunteg@naver.com>
2020-10-01 01:16:08 +0900

    201001 | algorithm

--- programmers/level2/temp.py
+++ programmers/level2/temp.py
@@ -1,28 +1,28 @@
-def solution(priorities, location):
+def solution(prior, location):
     answer = 0
-    N = len(priorities)
-    visit = [0] * N
-    print_list = []
-    i = 0
-    while sum(visit) != N:
-        for j in range(i, N):
-            if not visit[i] and not visit[j] and priorities[i] < priorities[j]:
-                print_list.append(j)
-                visit[j] = 1
+    idx = 0
+    prior_list = []
+    result = []
+    for i in range(len(prior)):
+        prior_list.append([i, prior[i]])
+
+    while len(prior_list):
+        p = prior_list[idx][1]
+        for q in prior_list:
+            if idx == q[0]:
+                continue
+            if p < q[1]:
+                prior_list.append(prior_list.pop(idx))
                 break
         else:
-            if not visit[i]:
-                print_list.append(i)
-                visit[i] = 1
-        i = (i+1) % N
+            result.append(prior_list.pop(idx))
 
-    for idx in range(len(print_list)):
-        if print_list[idx] == location:
-            answer = idx+1
-    print(print_list, answer)
+    for r in range(len(result)):
+        if location == result[r][0]:
+            answer = r+1
     return answer
 
 
 if __name__ == '__main__':
-    # solution([2, 1, 3, 2], 2)
-    solution([1, 1, 9, 1, 1, 1], 0)
+    print(solution([2, 1, 3, 2], 2))
+    print(solution([1, 1, 9, 1, 1, 1], 0))
