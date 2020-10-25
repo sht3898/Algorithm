8c6aa77ada4f731f29699ebd61174ab886bfdba4
sht3898 <sehyunteg@naver.com>
2020-10-25 19:20:52 +0900

    201025 | 타켓넘버

--- Programmers/level2/타겟넘버.py
+++ Programmers/level2/타겟넘버.py
@@ -1,11 +1,18 @@
-def solve(k, s):
-    if k == N:
-        return
-
 def solution(numbers, target):
     answer = 0
-    N = len(numbers)
-    visit = [0] * N
-    print(numbers)
-    print(target)
+    n = len(numbers)
+    def dfs(L, total):
+        nonlocal answer
+        if L == n:
+            if total == target:
+                answer += 1
+        else:
+            dfs(L+1, total+numbers[L])
+            dfs(L+1, total-numbers[L])
+    dfs(0, 0)
     return answer
+
+
+if __name__ == '__main__':
+    print(solution([1, 1, 1, 1, 1], 3)) # 5
+
