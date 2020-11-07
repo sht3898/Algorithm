23f9fbb3cc4f24a9aab02665493ad5f3a0b4f27d
sht3898 <sehyunteg@naver.com>
2020-11-08 03:00:26 +0900

    201108 | SQL

---
+++ SQL/STRING_DATE.sql
@@ -0,0 +1,30 @@
+-- 루시와 엘라 찾기
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE 
+FROM ANIMAL_INS
+WHERE NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+
+-- 이름에 el이 들어가는 동물 찾기
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE NAME LIKE '%el%'
+AND ANIMAL_TYPE = 'Dog'
+ORDER BY NAME
+
+-- 중성화 여부 파악하기
+-- 방법1
+SELECT ANIMAL_ID, NAME,
+    CASE WHEN SEX_UPON_INTAKE LIKE '%Neutered%' THEN 'O'
+         WHEN SEX_UPON_INTAKE LIKE '%Spayed%' THEN 'O'
+    ELSE 'X' END AS 중성화
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID
+
+-- 방법2
+SELECT ANIMAL_ID, NAME,
+    IF(SEX_UPON_INTAKE LIKE '%Neutered%' OR SEX_UPON_INTAKE LIKE '%Spayed%','O','X') AS 중성화
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID
+
+-- 오랜 기간 보호한 동물(2)
+
+-- DATETIME에서 DATE로 형 변환
