ea8052e103aacecc2d2b6dad4daebf4e47b9e0c5
sht3898 <sehyunteg@naver.com>
2021-01-27 18:05:50 +0900

    210127 | 백준-뱀

--- BOJ/samsung/3190_뱀.py
+++ BOJ/samsung/3190_뱀.py
@@ -1,16 +1,48 @@
-import sys;sys.stdin = open('3190_input.txt', 'r')
-import pprint
+import sys; sys.stdin = open('3190_input.txt', 'r')
+from collections import deque
+
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+
+
+def change(d, c):
+    if c == 'L':
+        d = (d-1) % 4
+    else:
+        d = (d+1) % 4
+    return d
+
+
+def bfs():
+    direct = 1
+    time = 1
+    x, y = 0, 0
+    snake = deque([[x, y]])
+    board[x][y] = 2
+    while True:
+        nx, ny = x+dx[direct], y+dy[direct]
+        if 0 <= nx < N and 0 <= ny < N and board[nx][ny] != 2:
+            if not board[nx][ny] == 1:
+                temp_x, temp_y = snake.popleft()
+                board[temp_x][temp_y] = 0
+            board[nx][ny] = 2
+            snake.append([nx, ny])
+            if time in rotate.keys():
+                direct = change(direct, rotate[time])
+            time += 1
+        else:
+            return time
+
 
 N = int(input())
 K = int(input())
-board = [[0]*(N+1) for _ in range(N+1)]
+board = [[0]*N for _ in range(N)]
 for _ in range(K):
     a, b = map(int, input().split())
-    board[a][b] = 1
-pprint.pprint(board)
-direct = [(0, 1), (1, 0), (-1, 0), (0, -1)]
-
+    board[a-1][b-1] = 1
 L = int(input())
-arr = [list(input().split()) for _ in range(L)]
-now = (1, 1)
-print(arr)
+rotate = {}
+for _ in range(L):
+    x, c = input().split()
+    rotate[int(x)] = c
+print(bfs())
