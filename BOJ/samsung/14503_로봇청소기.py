277c275af27373d33b79be902e19e58ac015e2e5
sht3898 <sehyunteg@naver.com>
2021-04-30 20:17:42 +0900

    210430 | 백준 - 삼성 기출 문제 풀이

--- BOJ/samsung/14503_로봇청소기.py
+++ BOJ/samsung/14503_로봇청소기.py
@@ -4,18 +4,45 @@ from collections import deque
 N, M = map(int, input().split())
 r, c, d = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
-visit = [[0]*M for _ in range(N)]
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 answer = 0
-print(board)
 Q = deque()
 Q.append([r, c])
 while Q:
     x, y = Q.popleft()
-    visit[x][y] = 1
-    d = (d-1)%4
-    nx, ny = x+dx[d], y+dy[d]
-    if 0 <= nx < N and 0 <= ny < M and not visit[nx][ny] and not board[nx][ny]:
-        Q.append([nx, ny])
-        answer += 1
+    board[x][y] = 1
+    print(x, y)
+    answer += 1
+    check = 0
+    for i in range(4):
+        d = (d-i-1) % 4
+        nx, ny = x+dx[d], y+dy[d]
+        if not board[nx][ny]:
+            Q.append([nx, ny])
+            check = 1
+            break
+    if check:
+        continue
+
+    if d == 0:
+        if not board[x+1][y]:
+            Q.append([x+1, y])
+        else:
+            break
+    elif d == 1:
+        if not board[x][y-1]:
+            Q.append([x, y-1])
+        else:
+            break
+    elif d == 2:
+        if not board[x-1][y]:
+            Q.append([x-1, y])
+        else:
+            break
+    elif d == 3:
+        if not board[x][y+1]:
+            Q.append([x, y+1])
+        else:
+            break
+print(answer)
