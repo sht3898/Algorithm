a1e194ee12ec8a82869869ec50442f4f4cf3ed81
sht3898 <sehyunteg@naver.com>
2021-06-15 00:31:07 +0900

    210615 | 프로그래머스 문제 | 프린터 - 우선순위 큐로 풀이

--- Programmers/level2/프린터.py
+++ Programmers/level2/프린터.py
@@ -18,6 +18,29 @@ def solution(pri, location):
     return answer
 
 
+import heapq
+
+
+def solution2(priorities, location):
+    answer = 1
+    arr = []
+    for i, p in enumerate(priorities):
+        heapq.heappush(arr, [-p, i])
+
+    while arr:
+        check = 0
+        for i in range(len(priorities)):
+            if priorities[i] == -arr[0][0]:
+                if i == location:
+                    check = 1
+                    break
+                heapq.heappop(arr)
+                answer += 1
+        if check:
+            break
+    return answer
+
+
 if __name__ == '__main__':
     print(solution([2, 1, 3, 2], 2))
     print(solution([1, 1, 9, 1, 1, 1], 0))
