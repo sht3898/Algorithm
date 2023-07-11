614043e2e06101785f4c1166afc4d7b4247ce916
sht3898 <sehyunteg@naver.com>
2023-07-11 23:57:02 +0900

    20230711 | 구슬탈출 풀이 중

--- BOJ/restart/13460_구슬탈출2.py
+++ BOJ/restart/13460_구슬탈출2.py
@@ -2,6 +2,24 @@ import sys;
 sys.stdin = open("13460_input.txt", "r")
 
 N, M = map(int, input().split())
-board = [list(input()) for _ in range(M)]
+blue = []
+red = []
+hole = []
+board = []
+# board = [list(input()) for _ in range(M)]
+for j in range(M):
+    temp = list(input())
+    board.append(temp)
+    for i in range(len(temp)):
+        if temp[i] == 'B':
+            blue.append([j, i])
+        elif temp[i] == 'R':
+            red.append([j, i])
+        elif temp[i] == 'O':
+            hole.append([j, i])
 answer = 0
 print(board)
+print(red, blue, hole)
+
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
